8ba5b563ce7f74d85cd81361a9124cfc93dd4459
rouhi assaf <rouhi621@gmail.com>
2024-06-02 22:29:45 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+document.querySelector(".userprofile h2").textContent="hello rouhi";
+console.log(document.querySelector(".userprofile h2"));
+console.log(document.querySelector(".userinfo p").classList.add("rouhi"));
